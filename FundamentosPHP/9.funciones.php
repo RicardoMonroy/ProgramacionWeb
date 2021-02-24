1084682013c1faedcea5ae19c62d8dee3e405c44
Ricardo Monroy <rmonroy.rodriguez@gmail.com>
2021-02-24 14:42:31 -0600

    Organizacion del curso en carpetas

---
+++ FundamentosPHP/9.funciones.php
@@ -0,0 +1,16 @@
+<?php
+
+function suma($uno, $dos){
+    $resultado = $uno + $dos;
+
+    return $resultado;
+}
+
+
+// $num1 = 4;
+// $num2 = 12;
+
+echo suma(4, 3);
+
+?>
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hola programadores
+La lista de tareas que harían de forma individual es:
++ En la calculadora metan a cada operación en una función.
